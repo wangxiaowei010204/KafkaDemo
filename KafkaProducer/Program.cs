0d010da750a89dd20ba41f30dc9b241a60165a78
wangxiaowei010204 <844503316@qq.com>
2020-04-07 21:26:48 +0800

    提交

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RdKafka;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brokerList = "localhost:9092";
+            string topicName = "test";
+
+            using (Producer producer = new Producer(brokerList))
+            {
+                using (Topic topic = producer.Topic(topicName))
+                {
+                    Console.WriteLine("{" + producer.Name + "} producing on {" + topic.Name + "}. q to exit.");
+
+                    string text;
+                    while ((text = Console.ReadLine()) != "q")
+                    {
+                        byte[] data = Encoding.UTF8.GetBytes(text);
+                        Task<DeliveryReport> deliveryReport = topic.Produce(data);
+                        var unused = deliveryReport.ContinueWith(task =>
+                        {
+                            Console.WriteLine("Partition: {" + task.Result.Partition + "}, Offset: {" + task.Result.Offset + "}");
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
