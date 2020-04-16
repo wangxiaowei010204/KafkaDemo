c88b5bfbc261b4db3b7c676de5bac1424ef5daf7
小伟 汪 <844503316@qq.com>
2020-04-16 16:50:52 +0800

    新增ConflumentKafka Sdk

---
+++ ConfluentKakfaProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace ConfluentKakfaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var p = new ProducerBuilder<string, string>(config).Build())
+            {
+                try
+                {
+                    string text;
+                    while ((text = Console.ReadLine()) != "q")
+                    {
+                        var dr = p.ProduceAsync("mytopic", new Message<string, string> {Key="Mykey", Value = text }).ConfigureAwait(false).GetAwaiter().GetResult();
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
